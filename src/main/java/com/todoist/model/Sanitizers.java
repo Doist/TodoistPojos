926a76907747cd01d05cc9b6854800ff30c76dbf
Gonçalo Silva <goncalossilva@gmail.com>
2016-03-01 17:41:59 +0000

    Make Sanitizers public

--- src/main/java/com/todoist/model/Sanitizers.java
+++ src/main/java/com/todoist/model/Sanitizers.java
@@ -3,9 +3,16 @@ package com.todoist.model;
 import java.util.regex.Pattern;
 
 public class Sanitizers {
-    static final Pattern PROJECT_NAME_INVALID_PATTERN = Pattern.compile("[<>\"=#+]+");
-    static final Pattern LABEL_NAME_INVALID_PATTERN = Pattern.compile("[<>\"=#+\\s%!?~:]+");
-    static final Pattern FILTER_NAME_INVALID_PATTERN = Pattern.compile("[<>\"=#+]+");
+    public static final String PROJECT_NAME_INVALID_CHARACTERS = "<>\"=#+";
+    public static final String LABEL_NAME_INVALID_CHARACTERS = "<>\"=#+\\s%!?~:";
+    public static final String FILTER_NAME_INVALID_CHARACTERS = "<>\"=#+";
 
-    static final String REPLACEMENT = "_";
+    public static final Pattern PROJECT_NAME_INVALID_PATTERN =
+            Pattern.compile("[" + PROJECT_NAME_INVALID_CHARACTERS + "]+");
+    public static final Pattern LABEL_NAME_INVALID_PATTERN =
+            Pattern.compile("[" + LABEL_NAME_INVALID_CHARACTERS + "]+");
+    public static final Pattern FILTER_NAME_INVALID_PATTERN =
+            Pattern.compile("[" + FILTER_NAME_INVALID_CHARACTERS + "]+");
+
+    public static final String REPLACEMENT = "_";
 }
