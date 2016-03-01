440449f45cc406098bda52795d116e3cc1e47655
Gonçalo Silva <goncalossilva@gmail.com>
2016-03-01 19:54:29 +0000

    Add @ as an invalid label name character

--- src/main/java/com/todoist/model/Sanitizers.java
+++ src/main/java/com/todoist/model/Sanitizers.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Sanitizers {
     public static final String PROJECT_NAME_INVALID_CHARACTERS = "<>\"=#+";
-    public static final String LABEL_NAME_INVALID_CHARACTERS = "<>\"=#+\\s%!?~:";
+    public static final String LABEL_NAME_INVALID_CHARACTERS = "<>\"=#+\\s%!?~:@";
     public static final String FILTER_NAME_INVALID_CHARACTERS = "<>\"=#+";
 
     public static final Pattern PROJECT_NAME_INVALID_PATTERN =
