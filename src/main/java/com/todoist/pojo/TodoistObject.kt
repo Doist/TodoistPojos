f387fc2d37c8055c1e8c1ce4442003d2e4a39daf
Pedro Santos <pedrodanielcsantos@gmail.com>
2019-04-17 11:09:18 +0100

    Migrate Section to kotlin

---
+++ src/main/java/com/todoist/pojo/TodoistObject.kt
@@ -0,0 +1,15 @@
+package com.todoist.pojo
+
+open class TodoistObject(open var id: Long, open var isDeleted: Boolean) {
+    open fun hasTempId() = id < 0
+
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> true
+            other == null || javaClass != other.javaClass -> false
+            else -> id == (other as TodoistObject).id
+        }
+    }
+
+    override fun hashCode() = (id xor id.ushr(32)).toInt()
+}
