687d9d95ed43a770cf60579ccbced4d67a0bfd7c
Pedro Santos <pedrodanielcsantos@gmail.com>
2019-04-25 12:22:17 +0100

    Convert Reminder to kotlin

---
+++ src/main/java/com/todoist/pojo/Reminder.kt
@@ -0,0 +1,45 @@
+package com.todoist.pojo
+
+open class Reminder<D: Due> @JvmOverloads constructor(
+        id: Long,
+        open var type: String?,
+        /** Exclusive to reminders of type [.TYPE_ABSOLUTE].  */
+        open var due: D?,
+        /** Exclusive to reminders of type [.TYPE_RELATIVE].  */
+        open var minuteOffset: Int?,
+        /** Exclusive to reminders of type [.TYPE_LOCATION].  */
+        open var name: String?,
+        /** Exclusive to reminders of type [.TYPE_LOCATION].  */
+        open var locLat: Double?,
+        /** Exclusive to reminders of type [.TYPE_LOCATION].  */
+        open var locLong: Double?,
+        /** Exclusive to reminders of type [.TYPE_LOCATION].  */
+        open var radius: Int?,
+        /** Exclusive to reminders of type [.TYPE_LOCATION].  */
+        open var locTrigger: String?,
+        open var notifyUid: Long?,
+        open var itemId: Long,
+        deleted: Boolean = false
+) : TodoistObject(id, deleted) {
+    open val isAbsolute: Boolean
+        get() = TYPE_ABSOLUTE == type
+
+    open val isRelative: Boolean
+        get() = TYPE_RELATIVE == type
+
+    open val isLocation: Boolean
+        get() = TYPE_LOCATION == type
+
+    companion object {
+        const val TYPE_ABSOLUTE = "absolute"
+        const val TYPE_RELATIVE = "relative"
+        const val TYPE_LOCATION = "location"
+
+        const val DEFAULT_MINUTE_OFFSET = 30
+
+        const val LOC_TRIGGER_ON_ENTER = "on_enter"
+        const val LOC_TRIGGER_ON_LEAVE = "on_leave"
+
+        const val MAX_COUNT = 700
+    }
+}
