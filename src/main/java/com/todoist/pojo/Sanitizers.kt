2dadbf37322d375609d10e5c2802665a8e5d1bb5
Pedro Santos <pedrodanielcsantos@gmail.com>
2019-05-23 18:20:29 +0100

    Convert Sanitizers to kotlin

---
+++ src/main/java/com/todoist/pojo/Sanitizers.kt
@@ -0,0 +1,15 @@
+package com.todoist.pojo
+
+import java.util.regex.Pattern
+
+object Sanitizers {
+    const val PROJECT_NAME_INVALID_CHARACTERS = "<>\"=#+"
+    const val LABEL_NAME_INVALID_CHARACTERS = "<>\"=#+\\s%!?~:@()"
+    const val FILTER_NAME_INVALID_CHARACTERS = "<>\"=#+"
+
+    @JvmField val PROJECT_NAME_INVALID_PATTERN = Pattern.compile("[$PROJECT_NAME_INVALID_CHARACTERS]+")
+    @JvmField val LABEL_NAME_INVALID_PATTERN = Pattern.compile("[$LABEL_NAME_INVALID_CHARACTERS]+")
+    @JvmField val FILTER_NAME_INVALID_PATTERN = Pattern.compile("[$FILTER_NAME_INVALID_CHARACTERS]+")
+
+    const val REPLACEMENT = "_"
+}
