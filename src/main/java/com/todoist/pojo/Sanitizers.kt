0eb892e2c6b9c8fdf7658b73bb2c0fd4f579b4e6
Chizoba Ogbonna <ogbonnacj2@gmail.com>
2020-06-17 19:01:45 +0100

    Update code style

--- src/main/java/com/todoist/pojo/Sanitizers.kt
+++ src/main/java/com/todoist/pojo/Sanitizers.kt
@@ -7,9 +7,12 @@ object Sanitizers {
     const val LABEL_NAME_INVALID_CHARACTERS = "<>\"=#+\\s%!?~:@()"
     const val FILTER_NAME_INVALID_CHARACTERS = "<>\"=#+"
 
-    @JvmField val PROJECT_NAME_INVALID_PATTERN = Pattern.compile("[$PROJECT_NAME_INVALID_CHARACTERS]+")
-    @JvmField val LABEL_NAME_INVALID_PATTERN = Pattern.compile("[$LABEL_NAME_INVALID_CHARACTERS]+")
-    @JvmField val FILTER_NAME_INVALID_PATTERN = Pattern.compile("[$FILTER_NAME_INVALID_CHARACTERS]+")
+    @JvmField
+    val PROJECT_NAME_INVALID_PATTERN = Pattern.compile("[$PROJECT_NAME_INVALID_CHARACTERS]+")
+    @JvmField
+    val LABEL_NAME_INVALID_PATTERN = Pattern.compile("[$LABEL_NAME_INVALID_CHARACTERS]+")
+    @JvmField
+    val FILTER_NAME_INVALID_PATTERN = Pattern.compile("[$FILTER_NAME_INVALID_CHARACTERS]+")
 
     const val REPLACEMENT = "_"
 }
--- src/main/java/com/todoist/pojo/Section.kt
+++ src/main/java/com/todoist/pojo/Section.kt
@@ -1,15 +1,15 @@
 package com.todoist.pojo
 
 open class Section @JvmOverloads constructor(
-        id: Long,
-        open var name: String,
-        open var projectId: Long,
-        open var sectionOrder: Int = 0,
-        open var isCollapsed: Boolean = false,
-        open var dateAdded: Long = 0,
-        open var isArchived: Boolean = false,
-        open var dateArchived: Long? = null,
-        isDeleted: Boolean = false
+    id: Long,
+    open var name: String,
+    open var projectId: Long,
+    open var sectionOrder: Int = 0,
+    open var isCollapsed: Boolean = false,
+    open var dateAdded: Long = 0,
+    open var isArchived: Boolean = false,
+    open var dateArchived: Long? = null,
+    isDeleted: Boolean = false
 ) : TodoistObject(id, isDeleted) {
     companion object {
         const val MIN_CHILD_ORDER = 1
