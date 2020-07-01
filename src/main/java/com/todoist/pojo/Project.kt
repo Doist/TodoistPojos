4738e8c46f763df01185c0c8596f176fbff4e375
Piotr Wilczek <piotrwilczek@gmail.com>
2020-07-01 16:27:51 +0300

    Project view style (#43)

--- src/main/java/com/todoist/pojo/Project.kt
+++ src/main/java/com/todoist/pojo/Project.kt
@@ -4,6 +4,7 @@ open class Project @JvmOverloads constructor(
         id: Long,
         name: String,
         open var color: Int = Colors.DEFAULT_COLOR_ID,
+        open var viewStyle: String = VIEW_STYLE_DEFAULT,
         open var parentId: Long?,
         open var childOrder: Int,
         open var isCollapsed: Boolean = false,
@@ -22,6 +23,10 @@ open class Project @JvmOverloads constructor(
     open val colorInt get() = Colors.getColor(color)
 
     companion object {
+        const val VIEW_STYLE_LIST = "list"
+        const val VIEW_STYLE_BOARD = "board"
+        const val VIEW_STYLE_DEFAULT = VIEW_STYLE_LIST
+
         const val MIN_CHILD_ORDER = 1
         const val MIN_DEPTH = 0
         const val MAX_DEPTH = 3
