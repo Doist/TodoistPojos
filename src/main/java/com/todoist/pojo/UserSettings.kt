9b2b40f5364e512d75d6c9ec671cf42524a8d80f
ab-helly <just.hellga@gmail.com>
2020-07-16 17:08:33 +0300

    Add soundOnComplete property to UserSettings

--- src/main/java/com/todoist/pojo/UserSettings.kt
+++ src/main/java/com/todoist/pojo/UserSettings.kt
@@ -3,5 +3,6 @@ package com.todoist.pojo
 open class UserSettings(
     open val reminderPush: Boolean,
     open val reminderDesktop: Boolean,
-    open val reminderEmail: Boolean
+    open val reminderEmail: Boolean,
+    open val soundOnComplete: Boolean
 )
