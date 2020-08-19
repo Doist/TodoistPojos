2a3b91b676ea725b7ecce75be5cf1a05a27e48ee
JinneeJ <julia.pustovoytova@gmail.com>
2020-08-19 16:23:54 +0300

    Remove Restriction from Features

--- src/main/java/com/todoist/pojo/Features.kt
+++ src/main/java/com/todoist/pojo/Features.kt
@@ -2,7 +2,6 @@ package com.todoist.pojo
 
 open class Features(
     open var text: String?,
-    open var restriction: Int,
     open var isDateistInlineDisabled: Boolean,
     open var dateistLang: String?,
     open var isGoldThemeEnabled: Boolean,
