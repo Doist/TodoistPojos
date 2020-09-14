60eeaf9ccef272e202ff0dfb07dc21f83d231556
Rastislav Vaško <rastislav.vasko@gmail.com>
2020-09-14 14:09:20 +0200

    Make Tooltips immutable

--- src/main/java/com/todoist/pojo/Tooltips.kt
+++ src/main/java/com/todoist/pojo/Tooltips.kt
@@ -1,6 +1,6 @@
 package com.todoist.pojo
 
 open class Tooltips(
-    open val scheduled: Set<String> = mutableSetOf(),
-    open val seen: Set<String> = mutableSetOf()
+    open val scheduled: Set<String> = emptySet(),
+    open val seen: Set<String> = emptySet()
 )
