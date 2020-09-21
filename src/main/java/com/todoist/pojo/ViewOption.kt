75a60a4286f0afdeb7ae446921a723e36dfded45
Pedro Santos <pedrodanielcsantos@gmail.com>
2020-09-21 18:39:27 +0100

    Add ViewOption model

---
+++ src/main/java/com/todoist/pojo/ViewOption.kt
@@ -0,0 +1,65 @@
+package com.todoist.pojo
+
+open class ViewOption(
+    id: Long,
+    open var viewType: Type,
+    open var objectId: Long?,
+    open var sortedBy: Sort?,
+    open var sortOrder: SortOrder?,
+    open var groupedBy: Group?,
+    open var filteredBy: String?,
+    isDeleted: Boolean
+) : TodoistObject(id, isDeleted) {
+    enum class Type(private val key: String) {
+        PROJECT("PROJECT"),
+        LABEL("LABEL"),
+        FILTER("FILTER");
+
+        override fun toString() = key
+
+        companion object {
+            fun get(typeKey: String): Type? = values().find { it.key == typeKey }
+        }
+    }
+
+    enum class Sort(private val key: String) {
+        ALPHABETICAL("ALPHABETICAL"),
+        ASSIGNEE("ASSIGNEE"),
+        ADDED_DATE("ADDED_DATE"),
+        DUE_DATE("DUE_DATE"),
+        PRIORITY("PRIORITY"),
+        PROJECT("PROJECT");
+
+        override fun toString() = key
+
+        companion object {
+            fun get(sortKey: String?): Sort? = values().find { it.key == sortKey }
+        }
+    }
+
+    enum class SortOrder(private val key: String) {
+        ASC("ASC"),
+        DESC("DESC");
+
+        override fun toString() = key
+
+        companion object {
+            fun get(sortKey: String?): SortOrder? = values().find { it.key == sortKey }
+        }
+    }
+
+    enum class Group(private val key: String) {
+        ASSIGNEE("ASSIGNEE"),
+        ADDED_DATE("ADDED_DATE"),
+        DUE_DATE("DUE_DATE"),
+        LABEL("LABEL"),
+        PRIORITY("PRIORITY"),
+        PROJECT("PROJECT");
+
+        override fun toString() = key
+
+        companion object {
+            fun get(groupKey: String?): Group? = values().find { it.key == groupKey }
+        }
+    }
+}
