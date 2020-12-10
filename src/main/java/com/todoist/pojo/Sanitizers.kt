ff6db083dbc1b9cf1623d4463133b83e875056fc
ab-helly <just.hellga@gmail.com>
2020-12-10 22:05:59 +0300

    Update name sanitizers and remove lint warnings
    Spec update PR: https://github.com/Doist/doist-documentation/pull/758

--- src/main/java/com/todoist/pojo/Sanitizers.kt
+++ src/main/java/com/todoist/pojo/Sanitizers.kt
@@ -3,18 +3,18 @@ package com.todoist.pojo
 import java.util.regex.Pattern
 
 object Sanitizers {
-    const val PROJECT_NAME_INVALID_CHARACTERS = "#\""
-    const val SECTION_NAME_INVALID_CHARACTERS = "/\""
-    const val LABEL_NAME_INVALID_CHARACTERS = "@\"\\s"
+    const val PROJECT_NAME_INVALID_CHARACTERS = "#\"()|&!,"
+    const val SECTION_NAME_INVALID_CHARACTERS = "/\"()|&!,"
+    const val LABEL_NAME_INVALID_CHARACTERS = "@\"\\s()|&!,"
 
     @JvmField
-    val PROJECT_NAME_INVALID_PATTERN = Pattern.compile("[$PROJECT_NAME_INVALID_CHARACTERS]+")
+    val PROJECT_NAME_INVALID_PATTERN: Pattern = Pattern.compile("[$PROJECT_NAME_INVALID_CHARACTERS]+")
 
     @JvmField
-    val SECTION_NAME_INVALID_PATTERN = Pattern.compile("[$SECTION_NAME_INVALID_CHARACTERS]+")
+    val SECTION_NAME_INVALID_PATTERN: Pattern = Pattern.compile("[$SECTION_NAME_INVALID_CHARACTERS]+")
 
     @JvmField
-    val LABEL_NAME_INVALID_PATTERN = Pattern.compile("[$LABEL_NAME_INVALID_CHARACTERS]+")
+    val LABEL_NAME_INVALID_PATTERN: Pattern = Pattern.compile("[$LABEL_NAME_INVALID_CHARACTERS]+")
 
     const val REPLACEMENT = "_"
 }
