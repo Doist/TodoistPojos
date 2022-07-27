0b1ceefafb3a12a9a6989f5fb5b4d76378ef2cd2
Pedro Santos <pedrodanielcsantos@users.noreply.github.com>
2022-07-27 12:22:35 +0100

    Add navigation customization models

---
+++ src/main/java/com/todoist/pojo/NavigationCustomization.kt
@@ -0,0 +1,5 @@
+package com.todoist.pojo
+
+open class NavigationCustomization<F : Feature>(
+    open val features: List<F>,
+)
--- src/main/java/com/todoist/pojo/UserSettings.kt
+++ src/main/java/com/todoist/pojo/UserSettings.kt
@@ -1,10 +1,14 @@
 package com.todoist.pojo
 
-open class UserSettings<Q : QuickAddCustomization<out Feature>>(
+open class UserSettings<
+    Q : QuickAddCustomization<out Feature>,
+    N : NavigationCustomization<out Feature>,
+    >(
     open val reminderPush: Boolean,
     open val reminderDesktop: Boolean,
     open val reminderEmail: Boolean,
     open val completedSoundDesktop: Boolean,
     open val completedSoundMobile: Boolean,
     open val quickAddCustomization: Q?,
+    open val navigationCustomization: N?,
 )
